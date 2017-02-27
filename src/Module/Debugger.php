c025db228930846abdc3235d73537a94940ed602
Darren Jacoby <darren@jacoby.co.za>
2017-02-27 09:45:46 +0200

    1.0.0-beta1

--- src/Module/Debugger.php
+++ src/Module/Debugger.php
@@ -32,6 +32,7 @@ class Debugger
      */
     public function route()
     {
+        if ($this->type === 'hierarchy') $this->hierarchy();
         if ($this->type === 'dump') $this->dump();
         if ($this->type === 'controller') $this->controller();
     }
@@ -58,6 +59,26 @@ class Debugger
         foreach ($this->data as $name => $item) {
             $item = (is_array($item) ? gettype($item) . '[' . count($item) . ']' : gettype($item));
             echo "<li>$" . $name . " &raquo; " . $item . "</li>";
+        }
+        echo '</ul></pre>';
+    }
+
+    /**
+     * Debug Hierarchy
+     *
+     * Return list of hierarchy
+     */
+    public function hierarchy()
+    {
+        global $wp_query;
+        $templates = (new \Brain\Hierarchy\Hierarchy())->getTemplates($wp_query);
+        $templates = array_reverse($templates);
+        $path = (has_filter('sober/controller/path') ? apply_filters('sober/controller/path', rtrim($path)) : get_stylesheet_directory() . '/controllers');
+        
+        echo '<pre><strong>Hierarchy Debugger:</strong><ul>';
+        foreach ($templates as $template) {
+            if (strpos($template, '.blade.php') || $template === 'index') continue;
+            echo "<li>" . basename($path) . '/' . $template . "</li>";
         }
         echo '</ul></pre>';
     }
