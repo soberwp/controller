550e2468f39e334e63ce53ebc9bf38ce158f82f2
Darren Jacoby <darren@jacoby.co.za>
2017-02-28 21:45:30 +0200

    1.0.0-beta1.2

--- controller.php
+++ controller.php
@@ -3,7 +3,7 @@
 Plugin Name:        Controller
 Plugin URI:         http://github.com/soberwp/controller
 Description:        WordPress plugin to enable a basic controller when using Blade with Sage 9
-Version:            1.0.0-beta1.1
+Version:            1.0.0-beta1.2
 Author:             Sober
 Author URI:         http://github.com/soberwp/
 License:            MIT License
@@ -39,10 +39,12 @@ function loader()
 
 function debugger()
 {
-    \App\sage('blade')->compiler()->directive('debug', function ($type) {
-        $debugger = ($type === '' ? 'controller' : $type);
-        return '<?php (new \Sober\Controller\Module\Debugger(get_defined_vars(), ' . $debugger . ')); ?>';
-    });
+    if (function_exists('\\App\\sage')) {
+        \App\sage('blade')->compiler()->directive('debug', function ($type) {
+            $debugger = ($type === '' ? 'controller' : $type);
+            return '<?php (new \Sober\Controller\Module\Debugger(get_defined_vars(), ' . $debugger . ')); ?>';
+        });
+    }
 }
 
 /**
